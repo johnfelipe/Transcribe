c42e69c918a1b9fce6dbd9ebc8f892f839971bb3
sashavol12@gmail.com <sashavol12@gmail.com>
2014-08-26 19:32:36 -0700

    StateManager stub

--- core/state/StateManager.java
+++ core/state/StateManager.java
@@ -1,6 +1,43 @@
 package core.state;
 
+import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
+
 public class StateManager {
+	private static ArrayList<State> states;
+	private static int position;
 	
+	private static class State {
+		private JSONObject stateObject;
+		
+		public static State capture() {
+			//TODO
+			return null;
+		}
+		
+		public void load() {
+			//TODO
+		}
+	}
 	
+	public static void push() {
+		//TODO
+	}
+	
+	public static void back() {
+		//TODO
+	}
+	
+	public static void forward() {
+		//TODO
+	}
+	
+	public static void end() {
+		//TODO
+	}
+	
+	public static void load() {
+		//TODO
+	}
 }
